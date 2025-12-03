90b5d7079a0ff9e9b4bfb2c085db52e55504f48c
DivayJain <djain_be24@thapar.edu>
2025-12-03 09:15:53 +0530

    Create 1024030294_Q2.cpp

---
+++ 1024030294_Q2.cpp
@@ -0,0 +1,37 @@
+//student_id 1024030294
+//Assignment 6 Q2 submitted
+
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+};
+
+void display(Node* head) {
+    if (!head) return;
+    Node* temp = head;
+    do {
+        cout << temp->data << " ";
+        temp = temp->next;
+    } while (temp != head);
+    cout << head->data << endl;
+}
+
+int main() {
+    Node* head = new Node{20, nullptr};
+    Node* second = new Node{100, nullptr};
+    Node* third = new Node{40, nullptr};
+    Node* fourth = new Node{80, nullptr};
+    Node* fifth = new Node{60, nullptr};
+
+    head->next = second;
+    second->next = third;
+    third->next = fourth;
+    fourth->next = fifth;
+    fifth->next = head;
+
+    display(head);
+    return 0;
+}
